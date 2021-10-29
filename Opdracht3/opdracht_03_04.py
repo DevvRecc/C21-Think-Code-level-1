c80d075ea5c07d920e5dda57a2635bed2394241b
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-10-29 13:17:10 +0200

    opdracht 5 python simpel code gemaakt

--- Opdracht3/opdracht_03_04.py
+++ Opdracht3/opdracht_03_04.py
@@ -10,6 +10,6 @@ print(Message) # show the message on the screen
 result = 2*3
 print("2*3 =", result)
 result = 5 - 3
-#print("5 - 3 =", result)
+print("5 - 3 =", result)
 
 print("Challenge complete!")
