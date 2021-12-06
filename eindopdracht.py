7f44f239db6b9d0e44c0bb22ee38b22ad5732579
Recep Yildiz <recep.yildiz@student.rocva.nl>
2021-12-06 11:06:42 +0100

    thinkcode afgerond

--- eindopdracht.py
+++ eindopdracht.py
@@ -1,50 +1,17 @@
-print("▒█░░▒█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀ 　 ▀▀█▀▀ █▀▀█ 　 ▒█▀▀█ █░░█ ▀▀█▀▀ █░░█ █▀▀█ █▀▀▄")
-print("▒█▒█▒█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀ 　 ░░█░░ █░░█ 　 ▒█▄▄█ █▄▄█ ░░█░░ █▀▀█ █░░█ █░░█")
-print("▒█▄▀▄█ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀ 　 ░░▀░░ ▀▀▀▀ 　 ▒█░░░ ▄▄▄█ ░░▀░░ ▀░░▀ ▀▀▀▀ ▀░░▀")
 
-def menu(): 
-    print("[1] Login")
-    print("[2] Rate my app!")
-    print("[0] Exit the program")
 
-users = {}
-status = ""
+print("Hi my name is droid, what is your name?")
+usertext = input("What is your name?")
+print("Nice to meet you", usertext)
 
-def newUser():
-    createLogin = input("Create login name: ")
- 
-    if createLogin in users:
-        print("\nLogin name already exist!\n")
-    else:
-        createPassw = input("Enter password: ")
-        users[createLogin] = createPassw
-        print("\nLogin successful!\n")
+print("I am feeling great today!")
+answer = input("How about you? ")
 
-def rate():
-    print("Hey how do you like my program?")
-    rateMe = input("Your Rating: ")
-    print("Thanks for your rating!")
-        
-
-menu()
-option = int(input("Enter your option: "))
-
-while option != 0:
-    if option == 1:
-        #hierbij inloggen
-        print("Optie 1 is gekozen")
-        newUser()
-    elif option == 2:
-        #optie 2 dingen
-        print("Optie 2 is gekozen")
-        rate()
-        pass
-    else:  
-        print("Invalid Option.")  
-
-    print()
-    menu()
-    option = int(input("Enter your option: "))                  
+if answer == "good":
+    print("That's good to hear!")
+else:
+    print("Oh no! That makes me sad!")
 
 print("Thanks for using my program!")
 #einde
+ 
